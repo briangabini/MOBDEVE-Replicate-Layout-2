9fae5bcc7466fefc2df200fb9133b0ad0cca4871
briangabini <briangabini@gmail.com>
2024-10-05 10:56:11 +0800

    feat: add files

---
+++ app/src/main/java/com/bgmobdeve/replicatelayout2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.bgmobdeve.replicatelayout2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Set status bar and navigation bar color
+        window.statusBarColor = getColor(R.color.blue_0288d1)
+//        window.navigationBarColor = getColor(R.color.blue_0288d1)
+    }
+}
